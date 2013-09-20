8b22bcbd31cf5ca469593548dfe86e4fec7a8d4b
florian <florian@florian-PearOS.(none)>
2013-09-20 16:03:40 +0200

    Add obd_console.py for console debugging of obd module.

---
+++ obd_console.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import obd_io
+import serial
+import platform
+import obd_sensors
+from datetime import datetime
+import time
+
+from obd_utils import scanSerial
+
+class OBD_Console():
+    def __init__(self):
+        self.port = None
+        
+    
+    def connect(self):
+        portnames = scanSerial()
+        #portnames = ['COM10']
+        print portnames
+        for port in portnames:
+            self.port = obd_io.OBDPort(port, None, 2, 2)
+            if(self.port.State == 0):
+                self.port.close()
+                self.port = None
+            else:
+                break
+
+        if(self.port):
+            print "Connected to "+self.port.port.name
+            
+    def is_connected(self):
+        return self.port
+        
+
+    def showOBDData(self):
+        if(self.port is None):
+            return None
+        
+        print "Showing current obd data:"
+        for index,  e in enumerate(obd_sensors.SENSORS):
+            (name,  value,  unit) = self.port.sensor(index)
+            print name,  value,  unit
+
+c = OBD_Console()
+c.connect()
+if(c.is_connected()):
+     c.showOBDData()
+    
